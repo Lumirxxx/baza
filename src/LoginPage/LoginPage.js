842ce3e1d228e37f979890d33316e233f4ad2e49
Lumirxxx <59173266+Lumirxxx@users.noreply.github.com>
2024-08-02 15:00:10 +0300

    Испрвлено
    Экран авторизации:
    1. Нет кнопки "Регистрация".+
    2. В дизайне другой шрифт.+
    3. Цвет inputов - неправильный.+
    4. Нет обводки у экрана авторизации.+
    5. Нет обводки у inputов+
    6. Цвет обводки при активном режиме inputов - неправильный.+
    7. Нет иконки просмотра пароля.+
    8. Некорректная анимация на кнопке "Войти". Сейчас там эффект нижней тени на кнопке, а должен быть другой.+
    9.  "Неверный логин или пароль" - должно появляться внизу отдельный всплывающим окном, а не под кнопкой "войти"+

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -2,13 +2,18 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { apiserver } from "../config";
-import { apiserverwiki } from "../config";
 
 const LoginPage = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
+    const [isErrorVisible, setIsErrorVisible] = useState(false);
     const navigate = useNavigate();
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -19,15 +24,14 @@ const LoginPage = () => {
                 password,
             })
             .then((response) => {
-                // Сохранение токена в локальном хранилище
                 localStorage.setItem("token", response.data.access);
                 localStorage.setItem("refreshToken", response.data.refresh);
                 navigate("/MainNews");
-                console.log("Вы успешно авторизовались!");
             })
             .catch((error) => {
-                console.log(error);
-                setErrorMessage("Неверный логин или пароль");
+                setErrorMessage("Неверный логин или пароль. Проверьте ввод данных.");
+                setIsErrorVisible(true);
+                setTimeout(() => setIsErrorVisible(false), 5000);
             });
     };
 
@@ -53,7 +57,7 @@ const LoginPage = () => {
                     <div className="login_form_button-container">
                         <input
                             className="select_button"
-                            placeholder="Логин"
+                            placeholder="Номер проекта"
                             type="text"
                             id="username"
                             value={username}
@@ -61,14 +65,25 @@ const LoginPage = () => {
                         />
                     </div>
                     <div className="login_form_button-container">
-                        <input
-                            className="select_button select_button_password_margin"
-                            placeholder="Пароль"
-                            type="password"
-                            id="password"
-                            value={password}
-                            onChange={(event) => setPassword(event.target.value)}
-                        />
+                        <div className="password-input-wrapper">
+                            <input
+                                className="select_button select_button_password_margin"
+                                placeholder="Пароль"
+                                type={showPassword ? "text" : "password"}
+                                id="password"
+                                value={password}
+                                onChange={(event) => setPassword(event.target.value)}
+                            />
+                            {password.length > 0 && (
+                                <div className="icon-container" onClick={togglePasswordVisibility}>
+                                    {showPassword ? (
+                                        <img className="show-password" src="/eye-icon.svg" alt="Скрыть пароль" />
+                                    ) : (
+                                        <img className="show-password" src="/eye-off-icon.svg" alt="Показать пароль" />
+                                    )}
+                                </div> 
+                            )}
+                        </div>
                     </div>
                     <div className="login_form_button-container-submit">
                         <button
@@ -80,9 +95,19 @@ const LoginPage = () => {
                             Войти
                         </button>
                     </div>
-                    {errorMessage && <p className="error-message_login">{errorMessage}</p>}
+                    <div className="registration-link">
+                        <a href="#registration">Регистрация</a>
+                    </div>
                 </form>
             </div>
+            {errorMessage && (
+                <div className={`error-popup ${isErrorVisible ? 'show' : 'hide'}`}>
+                    <div onClick={() => setIsErrorVisible(false)} className="error-icon">
+                        <img src="/error-icon.svg" alt="Error" />
+                    </div>
+                    <div className="error-text">{errorMessage}</div>
+                </div>
+            )}
         </div>
     );
 };
